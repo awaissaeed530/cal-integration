539fb489512c871a6adf5b056c22242073fb4614
Awais Saeed <awaissaeed530@gmail.com>
2022-08-08 03:05:06 +0500

    update course structure

--- src/models/ICourse.ts
+++ src/models/ICourse.ts
@@ -1,10 +1,28 @@
 export interface ICourse {
   title: string;
   location: string;
-  // '10:30 PM' format
+
+  /** Day on which class is held */
+  weekday: WeekDay;
+
+  /** Class session time '10:30 PM' format */
   session_time: string;
+
+  /** Is the event recurring */
   is_recurring: boolean;
+
+  /** Course start date */
   start_date: Date;
+  /** Course end date */
   end_date: Date;
   description: string;
 }
+
+export type WeekDay =
+  | "Monday"
+  | "Tuesday"
+  | "Wednesday"
+  | "Thursday"
+  | "Friday"
+  | "Saturday"
+  | "Sunday";
