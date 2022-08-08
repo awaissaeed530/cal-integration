432c76d9e44e78e8cb8eaecd925c85e4a162e31b
Awais Saeed <awaissaeed530@gmail.com>
2022-08-08 16:29:16 +0500

    add export all option

--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -39,39 +39,52 @@ function Courses() {
   };
 
   const exportToOutlook = (event: CourseEvent) => {
-    generateIcs(event.courseRef, event.date);
+    generateIcs([event]);
+  };
+
+  const exportAll = () => {
+    generateIcs(events);
   };
 
   return (
-    <div className="grid grid-cols-4 gap-4 p-8">
-      {events.map((event) => (
-        <div
-          className="p-4 bg-white rounded-md shadow-lg"
-          key={event.courseRef.title}
-        >
-          <div className="flex items-center justify-between">
-            <div className="text-lg font-medium">{event.courseRef.title}</div>
-            <div className="text-sm text-gray-400">
-              {format(event.date, "dd/MM/yyyy")}
+    <div className="p-8">
+      <button
+        className="px-2 py-1 mt-2 mb-4 border rounded"
+        onClick={exportAll}
+      >
+        Export All
+      </button>
+
+      <div className="grid grid-cols-4 gap-4">
+        {events.map((event) => (
+          <div
+            className="p-4 bg-white rounded-md shadow-lg"
+            key={event.courseRef.title}
+          >
+            <div className="flex items-center justify-between">
+              <div className="text-lg font-medium">{event.courseRef.title}</div>
+              <div className="text-sm text-gray-400">
+                {format(event.date, "dd/MM/yyyy")}
+              </div>
             </div>
+            <div className="text-sm text-gray-500">
+              {event.courseRef.description}
+            </div>
+            <button
+              className="px-2 py-1 mt-2 border rounded"
+              onClick={() => exportToOutlookWeb(event)}
+            >
+              Export To Outlook Web
+            </button>
+            <button
+              className="px-2 py-1 mt-2 border rounded"
+              onClick={() => exportToOutlook(event)}
+            >
+              Export To Outlook
+            </button>
           </div>
-          <div className="text-sm text-gray-500">
-            {event.courseRef.description}
-          </div>
-          <button
-            className="px-2 py-1 mt-2 border rounded"
-            onClick={() => exportToOutlookWeb(event)}
-          >
-            Export To Outlook Web
-          </button>
-          <button
-            className="px-2 py-1 mt-2 border rounded"
-            onClick={() => exportToOutlook(event)}
-          >
-            Export To Outlook
-          </button>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
--- src/utils/ics.utils.ts
+++ src/utils/ics.utils.ts
@@ -1,43 +1,51 @@
-import { ICourse } from "../models";
+import { CourseEvent } from "../models";
 import { formatDateWithSessionTime } from "./events.utils";
 
-export function generateIcs(course: ICourse, eventDay: Date) {
-  const start = normalizeDate(
-    formatDateWithSessionTime(course.start_time, eventDay)
-  );
-  const end = normalizeDate(
-    formatDateWithSessionTime(course.end_time, eventDay)
-  );
-
+export function generateIcs(events: CourseEvent[]) {
   const ics_lines = ["BEGIN:VCALENDAR"];
   ics_lines.push("PRODID:-//Pioneers Education//Pioneers Education v1.0//EN");
   ics_lines.push("VERSION:2.0");
   ics_lines.push("CALSCALE:GREGORIAN");
-  ics_lines.push("BEGIN:VEVENT");
-  ics_lines.push("UID:Pioneers Education");
-  ics_lines.push(
-    "DTSTAMP:" + start,
-    "DTSTART:" + start,
-    "DTEND:" + end,
-    "SUMMARY:" + course.title.replace(/.{65}/g, "$&\r\n ") // making sure it does not exceed 75 characters per line
-  );
-  if (course.description !== null && course.description !== "") {
-    ics_lines.push("X-ALT-DESC;FMTTYPE=text/html:" + course.description);
-  }
-  if (course.location !== null && course.location !== "") {
-    ics_lines.push("LOCATION:" + course.location);
-  }
-  ics_lines.push(
-    "STATUS:CONFIRMED",
-    "LAST-MODIFIED:" + normalizeDate(new Date()),
-    "SEQUENCE:0",
-    "END:VEVENT",
-    "END:VCALENDAR"
-  );
+
+  events.forEach((event) => {
+    const start = normalizeDate(
+      formatDateWithSessionTime(event.courseRef.start_time, event.date)
+    );
+    const end = normalizeDate(
+      formatDateWithSessionTime(event.courseRef.end_time, event.date)
+    );
+
+    ics_lines.push("BEGIN:VEVENT");
+    ics_lines.push("UID:Pioneers Education");
+    ics_lines.push(
+      "DTSTAMP:" + start,
+      "DTSTART:" + start,
+      "DTEND:" + end,
+      "SUMMARY:" + event.courseRef.title.replace(/.{65}/g, "$&\r\n ") // making sure it does not exceed 75 characters per line
+    );
+    if (
+      event.courseRef.description !== null &&
+      event.courseRef.description !== ""
+    ) {
+      ics_lines.push(
+        "X-ALT-DESC;FMTTYPE=text/html:" + event.courseRef.description
+      );
+    }
+    if (event.courseRef.location !== null && event.courseRef.location !== "") {
+      ics_lines.push("LOCATION:" + event.courseRef.location);
+    }
+    ics_lines.push(
+      "STATUS:CONFIRMED",
+      "LAST-MODIFIED:" + normalizeDate(new Date()),
+      "SEQUENCE:0",
+      "END:VEVENT"
+    );
+  });
+  ics_lines.push("END:VCALENDAR");
   let dlurl =
     "data:text/calendar;charset=utf-8," +
     encodeURIComponent(ics_lines.join("\r\n"));
-  const filename = course.title;
+  const filename = "Event Calendar";
 
   try {
     const save = document.createElement("a");
